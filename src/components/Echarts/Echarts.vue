4672bd51475a77efcaff8d5c60811ef6c20dce63
PC-PC\PC <zhuyuanwei@elink-cn.com>
2018-04-04 10:29:08 +0800

    一键式注册组件

--- src/components/Echarts/Echarts.vue
+++ src/components/Echarts/Echarts.vue
@@ -42,27 +42,27 @@
 </template>
 
 <script>
-import HomeEcharts1 from './component/HomeEcharts1'
-import HomeEcharts2 from './component/HomeEcharts2'
+// import HomeEcharts1 from './component/HomeEcharts1'
+// import HomeEcharts2 from './component/HomeEcharts2'
 
-import KanbanEcharts1 from './component/KanbanEcharts1'
-import KanbanEcharts2 from './component/KanbanEcharts2'
-import KanbanEcharts3 from './component/KanbanEcharts3'
-import KanbanEcharts4 from './component/KanbanEcharts4'
+// import KanbanEcharts1 from './component/KanbanEcharts1'
+// import KanbanEcharts2 from './component/KanbanEcharts2'
+// import KanbanEcharts3 from './component/KanbanEcharts3'
+// import KanbanEcharts4 from './component/KanbanEcharts4'
 
-import ShequEcharts1 from './component/ShequEcharts1'
-import ShequEcharts2 from './component/ShequEcharts2'
-import ShequEcharts3 from './component/ShequEcharts3'
-import ShequEcharts4 from './component/ShequEcharts4'
-import ShequEcharts5 from './component/ShequEcharts5'
-import ShequEcharts6 from './component/ShequEcharts6'
-import ShequEcharts7 from './component/ShequEcharts7'
+// import ShequEcharts1 from './component/ShequEcharts1'
+// import ShequEcharts2 from './component/ShequEcharts2'
+// import ShequEcharts3 from './component/ShequEcharts3'
+// import ShequEcharts4 from './component/ShequEcharts4'
+// import ShequEcharts5 from './component/ShequEcharts5'
+// import ShequEcharts6 from './component/ShequEcharts6'
+// import ShequEcharts7 from './component/ShequEcharts7'
 
-import KefangEcharts1 from './component/KefangEcharts1'
-import KefangEcharts2 from './component/KefangEcharts2'
-import KefangEcharts3 from './component/KefangEcharts3'
-import KefangEcharts4 from './component/KefangEcharts4'
-
+// import KefangEcharts1 from './component/KefangEcharts1'
+// import KefangEcharts2 from './component/KefangEcharts2'
+// import KefangEcharts3 from './component/KefangEcharts3'
+// import KefangEcharts4 from './component/KefangEcharts4'
+import './component/global.js'
 export default {
     name: 'testEcharts',
     data () {
@@ -79,23 +79,23 @@ export default {
         }
     },
     components: {
-        HomeEcharts1,
-        HomeEcharts2,
-        KanbanEcharts1,
-        KanbanEcharts2,
-        KanbanEcharts3,
-        KanbanEcharts4,
-        ShequEcharts1,
-        ShequEcharts2,
-        ShequEcharts3,
-        ShequEcharts4,
-        ShequEcharts5,
-        ShequEcharts6,
-        ShequEcharts7,
-        KefangEcharts1,
-        KefangEcharts2,
-        KefangEcharts3,
-        KefangEcharts4
+        // HomeEcharts1,
+        // HomeEcharts2,
+        // KanbanEcharts1,
+        // KanbanEcharts2,
+        // KanbanEcharts3,
+        // KanbanEcharts4,
+        // ShequEcharts1,
+        // ShequEcharts2,
+        // ShequEcharts3,
+        // ShequEcharts4,
+        // ShequEcharts5,
+        // ShequEcharts6,
+        // ShequEcharts7,
+        // KefangEcharts1,
+        // KefangEcharts2,
+        // KefangEcharts3,
+        // KefangEcharts4
     }
 }
 </script>
