06ec8ed8ecac5396790808d84040a07497ba37ce
朱元伟 <1163929661@qq.com>
2018-05-15 10:23:37 +0800

    测试github与码云

--- src/components/testEcharts/testEcharts.vue
+++ src/components/testEcharts/testEcharts.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+      <!-- 测试github与码云 -->
     <div class="TestEcharts1"><TestEcharts1></TestEcharts1></div>
     <div class="TestEcharts2"><TestEcharts2></TestEcharts2></div>
     <div class="TestEcharts3"><TestEcharts3 :heightDiv="heightDiv2"></TestEcharts3></div>
